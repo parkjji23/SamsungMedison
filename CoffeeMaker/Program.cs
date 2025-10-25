34b4255a4ba25fda4ee998362cbe06e9a4c4e7c0
jep <parkjji23@naver.com>
2025-10-25 14:49:28 +0900

    feat: 커피머신 프로젝트 신규생성

--- CoffeeMaker/Program.cs
+++ CoffeeMaker/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CoffeeMaker;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var machine = new CoffeeMachine(beans: 10, water: 10, milk: 10);
+        try
+        {
+            machine.MakeCoffee("Latte");
+            machine.MakeCoffee("Americano");
+            machine.MakeCoffee("Latte"); // Should succeed
+            machine.MakeCoffee("Latte"); // Should fail due to lack of ingredients
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
